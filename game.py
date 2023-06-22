fcaa9317031fa5fbcd1eaf9fd96124c9244cd566
SHlit <52101743+Ltvsh16@users.noreply.github.com>
2023-06-22 17:04:01 +0600

    Innit commit

--- game.py
+++ game.py
@@ -5,8 +5,8 @@ count = 0
 while True:
     count += 1
     predict_number = int(input("Угадай число от 1 до 100"))
-    
-    if predict_number > number:3
+
+    if predict_number > number:
         print("Число должно быть меньше!")
 
     elif predict_number < number:
